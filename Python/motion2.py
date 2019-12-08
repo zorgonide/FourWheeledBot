b9c6efc196f899a432fa30489e523121cb8c0744
zorgonide <ammar1998@gmail.com>
2019-12-08 11:12:25 +0530

    Improved code for turning while moving

---
+++ Python/motion2.py
@@ -0,0 +1,62 @@
+#Improved code usind WASD and JL
+import tty, termios, sys
+from serial import Serial
+from time import sleep # Import the sleep function from the time module
+import time
+i = 0
+while(1):
+        try:    
+                ser = Serial('COM'+str(i), 19200, timeout=.1) #PC
+                # ser = Serial('/dev/ttyACM'+str(i), 19200, timeout=.1) #UNIX
+                break
+        except:
+                print('COM'+str(i)+ ' ERROR')
+                i+=1
+                
+
+def getchar():
+   #Returns a single character from standard input
+   fd = sys.stdin.fileno()
+   old_settings = termios.tcgetattr(fd)
+   try:
+      tty.setraw(sys.stdin.fileno())
+      ch = sys.stdin.read(1)
+   finally:
+      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+   return ch
+# time.sleep(2)   
+while 1:
+    ch = getchar()
+    if ch.strip() == '':
+        print('bye!')
+        break
+    else:
+        # print(ch)
+        if ch=='w':
+                print ("up")
+                ser.write(b'w')
+                sleep(0.5)
+        elif ch=='s':
+                print ("down")
+                ser.write(b's')
+                sleep(0.5)
+        elif ch=='d':
+                print ("right")
+                ser.write(b'a')
+                sleep(0.25)
+        elif ch=='a':
+                print ("left")
+                ser.write(b'd')
+                sleep(0.25)
+        elif ch=='j':
+                print ("forwardleft")
+                ser.write(b'd')
+                sleep(0.1)
+        elif ch=='l':
+                print ("forwardright")
+                ser.write(b'd')
+                sleep(0.1)
+        elif ch=='t':
+                print ("trot")
+                ser.write(b't')
+                sleep(0.1)
