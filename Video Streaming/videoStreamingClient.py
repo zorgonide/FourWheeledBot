fe3c6232cca7f44754a5c5fa56b6c327a85df6d1
zorgonide <ammar1998@gmail.com>
2019-12-09 12:23:30 +0530

    Added Video streaming client script

---
+++ Video Streaming/videoStreamingClient.py
@@ -0,0 +1,28 @@
+import socket
+import time
+import picamera
+
+# Connect a client socket to my_server:8000 (change my_server to the
+# hostname of your server)
+client_socket = socket.socket()
+client_socket.connect(('192.168.43.163', 8000))
+
+# Make a file-like object out of the connection
+connection = client_socket.makefile('wb')
+try:
+    camera = picamera.PiCamera()
+    camera.resolution = (480, 360)
+    camera.framerate = 20
+    # Start a preview and let the camera warm up for 2 seconds
+    camera.start_preview()
+    time.sleep(2)
+    # Start recording, sending the output to the connection for 60
+    # seconds, then stop
+    camera.start_recording(connection, format='h264')
+    #camera.wait_recording(90)
+    i = input("Enter q to quit")
+    if ( i == 'q' ):
+        camera.stop_recording()
+finally:
+    connection.close()
+    client_socket.close()
