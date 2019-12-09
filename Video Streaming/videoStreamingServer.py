365c7d7849bb6cd1cc43b9aa39459eeee1aa81f9
zorgnide <ammar1998@gmail.com>
2019-12-09 12:37:14 +0530

    Added server script

---
+++ Video Streaming/videoStreamingServer.py
@@ -0,0 +1,32 @@
+import socket
+import subprocess
+
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+server_socket = socket.socket()
+server_socket.bind(('', 8000))
+server_socket.listen(0)
+# print(1)
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+try:
+    # Run a viewer with an appropriate command line. Uncomment the mplayer
+    # version if you would prefer to use mplayer instead of VLC
+    # cmdline = ['vlc', '--demux', 'h264', '-']
+    cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
+    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+    # print(2)
+    while True:
+        # Repeatedly read 1k of data from the connection and write it to
+        # the media player's stdin
+        data = connection.read(1024)
+        # print(3)
+        if not data:
+            # print(5)
+            break
+        player.stdin.write(data)
+finally:
+    # print(4)
+    connection.close()
+    server_socket.close()
+    player.terminate()
